91e019785a616408de61e922f4046ccc6181822f
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-03-07 15:34:47 -0300

    feat: added listing of all followers of a user

--- followers/views.py
+++ followers/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import get_object_or_404
 from .models import Followers
 from users.models import User
 from .serializers import FollowerSerializer
-from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView, Request, Response, status
+import ipdb
 
 
-class FollowerView(CreateAPIView, DestroyAPIView):
+class FollowerView(ListAPIView, CreateAPIView, DestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Followers.objects.all()
     serializer_class = FollowerSerializer
+
+    def get_queryset(self):
+        user = get_object_or_404(User, pk=self.kwargs.get("user_follow_id", None))
+        return Followers.objects.filter(user_id=user)
 
     def perform_create(self, serializer):
         user_follow = get_object_or_404(
