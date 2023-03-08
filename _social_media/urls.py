6bf69d49954e053c3b7c9f6fa76f1b96209fa8c2
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-03-08 10:53:05 -0300

    feat: followers crud and seializer finished

--- _social_media/urls.py
+++ _social_media/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("publications.urls")),
+    path("api/", include("followers.urls")),
 ]
