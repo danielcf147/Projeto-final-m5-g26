ccc0e92f046f30bb1614f740cc425a227f310fb1
Matheus Vicente <matheusvicente112@outlook.com>
2023-03-09 15:29:53 -0300

    feat:Like comments and url

--- publications/views.py
+++ publications/views.py
@@ -4,7 +4,10 @@ from .models import Publication, Comment
 from .serializers import PublicationSerializer, CommentSerializer
 from .permissions import IsAccountOwner
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from publications.models import Comment, Like
 
 class PublicationView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -43,3 +46,16 @@ class CommentDetailView(RetrieveUpdateDestroyAPIView):
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class CommentLikeView(View):
+
+    def post(self, request, *args, **kwargs):
+        comment_id = kwargs.get("pk")
+        comment = get_object_or_404(Comment, pk=comment_id)
+        user = request.user
+
+        if Like.objects.filter(comment=comment, user=user).exists():
+            return JsonResponse({"success": False, "error": "You already liked this comment"})
+
+        Like.objects.create(comment=comment, user=user)
+        return JsonResponse({"success": True})
