16e9c26901f04319e8d4ce9b7651551c125ed530
Matheus Vicente <matheusvicente112@outlook.com>
2023-03-12 11:54:33 -0300

    fix:add serializer

--- publications/urls.py
+++ publications/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path("publication/", views.PublicationView.as_view()),
     path("publication/timeline/", views.TimeLinePublicationView.as_view()),
     path("publication/<int:pk>/", views.PublicationDetailView.as_view()),
-    path("publication/<int:pk>/like/", views.PublicacaoLikeView.as_view()),
     path("comment/<int:pk>/", views.CommentView.as_view()),
     path("comment/RUD/<int:pk>/", views.CommentDetailView.as_view()),
     path("comment/<int:pk>/like/", views.CommentLikeView.as_view()),
+    path("publication/<int:pk>/like/", views.PublicationLikeView.as_view()),
 
 ]
